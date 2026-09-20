79ed8294f3bc93b4df1c5510c439c4980d70b6e8
Mdnurullahcce7841 <mdnurullahcce@gmail.com>
2026-09-20 22:57:30 +0600

    Installed vite react project and install all necessary tools, like: tailwaidcss, daisyUI and Toastify

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ToastContainer } from 'react-toastify';
+
+const App = () => {
+  return (
+    <div>
+    
+      
+      <ToastContainer></ToastContainer>
+    </div>
+  );
+};
+
+export default App;
