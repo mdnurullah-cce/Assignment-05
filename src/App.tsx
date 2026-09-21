bf6548a58c9ce372e4bec3fbcc8a92a5258c14ef
Mdnurullahcce7841 <mdnurullahcce@gmail.com>
2026-09-21 22:57:20 +0600

    Updated type of technologies

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 
 import { ToastContainer } from 'react-toastify';
 import Navbar from './Components/Navbar';
 import Hero from './Components/Hero';
+import Technologies from './Components/Technologies';
+import type { ITechnology } from './Types/Technologies';
 
-const App = () => {
+const techPromiseFetch = async ():Promise<ITechnology[]> => {
+  const res =await fetch("/data.json")
+  const data = await res.json()
+  return data;
+}
+
+function App() {
+  const techPromise = techPromiseFetch();
+  
   return (
     <div>
     <Navbar />
     <Hero />
+    <Suspense fallback={<p>Loading...</p>}> 
+    <Technologies techPromise={techPromise} />
+    </Suspense>
+    
+    
+    
+    
       
       <ToastContainer></ToastContainer>
     </div>
--- src/Components/Technologies.tsx
+++ src/Components/Technologies.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { use } from 'react';
+import type { ITechnology } from '../Types/Technologies';
 
-const Technologies = () => {
+
+interface TechnologiesProps {
+    techPromise: Promise<ITechnology[]>;
+}
+
+const Technologies = ({ techPromise}:TechnologiesProps) => {
+    console.log(techPromise);
+    const Technologies = use(techPromise);
+    console.log(Technologies, "technologiesddd");
     return (
         <div>
             <>
-            
+
             </>
         </div>
     );
